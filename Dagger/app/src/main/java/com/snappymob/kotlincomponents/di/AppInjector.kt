b651282de9c05f3494d137fce69eaa5f02285aea
ahmedrizwan <ahmedrizwan@outlook.com>
2017-09-11 21:47:45 +0800

    Convert AppInjector to Kotlin

---
+++ Dagger/app/src/main/java/com/snappymob/kotlincomponents/di/AppInjector.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.snappymob.kotlincomponents.di
+
+import android.app.Activity
+import android.app.Application
+import android.arch.lifecycle.LifecycleActivity
+import android.os.Bundle
+
+import com.snappymob.kotlincomponents.App
+
+import dagger.android.AndroidInjection
+
+object AppInjector {
+
+    fun init(app: App) {
+        DaggerAppComponent.builder()
+                .application(app)
+                .build()
+                .inject(app)
+
+        app.registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle) {
+                handleActivity(activity)
+            }
+
+            override fun onActivityStarted(activity: Activity) {
+
+            }
+
+            override fun onActivityResumed(activity: Activity) {
+
+            }
+
+            override fun onActivityPaused(activity: Activity) {
+
+            }
+
+            override fun onActivityStopped(activity: Activity) {
+
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+
+            }
+
+            override fun onActivityDestroyed(activity: Activity) {
+
+            }
+        })
+    }
+
+    private fun handleActivity(activity: Activity) {
+        if (activity is LifecycleActivity) {
+            AndroidInjection.inject(activity)
+        }
+    }
+}
