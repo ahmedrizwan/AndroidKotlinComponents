ca2b5b995ac67a0a5d7299ac7776e635e99844a9
ahmedrizwan <ahmedrizwan@outlook.com>
2017-10-04 15:27:38 +0800

    Update dagger viewModel to match Google sample

--- Dagger/app/src/main/java/com/snappymob/kotlincomponents/viewmodel/RepoViewModel.kt
+++ Dagger/app/src/main/java/com/snappymob/kotlincomponents/viewmodel/RepoViewModel.kt
@@ -1,11 +1,14 @@
 package com.snappymob.kotlincomponents.viewmodel
 
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
 import android.arch.lifecycle.ViewModel
-import android.util.ArrayMap
 import com.snappymob.kotlincomponents.model.Repo
+import com.snappymob.kotlincomponents.network.AbsentLiveData
 import com.snappymob.kotlincomponents.network.Resource
 import com.snappymob.kotlincomponents.repository.RepoRepository
+import java.util.*
 import javax.inject.Inject
 
 
@@ -15,14 +18,30 @@ import javax.inject.Inject
 
 class RepoViewModel
 @Inject constructor(repository: RepoRepository) : ViewModel() {
-    val repoRepository = repository
-    val map: ArrayMap<String, LiveData<Resource<List<Repo>>>> = ArrayMap()
+//    val repoRepository = repository
+//    val map: ArrayMap<String, LiveData<Resource<List<Repo>>>> = ArrayMap()
     var currentRepoUser: String? = null
-    fun loadRepos(username: String): LiveData<Resource<List<Repo>>>? {
-        currentRepoUser = username
-        if (map[username] == null) {
-            map[username] = repoRepository.loadRepos(username)
-        }
-        return map[username]
+
+    //live data of list of Repos, called results
+    val results: LiveData<Resource<List<Repo>>>
+    private val query: MutableLiveData<String> = MutableLiveData()
+
+    init {
+        results = Transformations.switchMap(query, {
+            when {
+                it == null || it.length == 1 -> AbsentLiveData.create()
+                else -> repository.loadRepos(it)
+            }
+        })
     }
+
+    fun setQuery(originalInput: String?) {
+        if(originalInput==null) return
+        val input = originalInput.toLowerCase(Locale.getDefault()).trim { it <= ' ' }
+        if (Objects.equals(input, query.value)) {
+            return
+        }
+        query.value = input
+    }
+
 }
