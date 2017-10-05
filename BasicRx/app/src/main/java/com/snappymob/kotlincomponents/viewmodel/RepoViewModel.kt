8ce57e2d392b78a595c8280953feac7a231ffd6c
ahmedrizwan <ahmedrizwan@outlook.com>
2017-10-05 14:43:17 +0800

    Update viewModel in include LiveData for Query String

--- BasicRx/app/src/main/java/com/snappymob/kotlincomponents/viewmodel/RepoViewModel.kt
+++ BasicRx/app/src/main/java/com/snappymob/kotlincomponents/viewmodel/RepoViewModel.kt
@@ -1,24 +1,40 @@
 package com.snappymob.kotlincomponents.viewmodel
 
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Transformations
 import android.arch.lifecycle.ViewModel
 import android.util.ArrayMap
+import com.snappymob.kotlincomponents.network.AbsentLiveData
 import com.snappymob.kotlincomponents.repository.RepoRepository
 
 
 /**
  * Created by ahmedrizwan on 9/10/17.
  */
-class RepoViewModel(repository: RepoRepository):ViewModel() {
-    val repoRepository = repository
+class RepoViewModel constructor(val repoRepository: RepoRepository):ViewModel() {
+
     val map: ArrayMap<String, RepoLiveData> = ArrayMap()
-    var currentRepoUser: String? = null
+
+    var query: MutableLiveData<String> = MutableLiveData()
+
+    init {
+       Transformations.switchMap(query,
+               { search ->
+                   if(search==null || search.trim().length==0){
+                        AbsentLiveData.create<RepoLiveData>()
+                   } else {
+                       repoRepository.loadRepos(search)
+                   }
+               }
+       )
+    }
+
     fun loadRepos(username: String): RepoLiveData? {
-        currentRepoUser = username
-        if (map[username] == null) {
-            map[username] = RepoLiveData(repoRepository, owner = username)
+        query = username
+        if (map[query] == null) {
+            map[query] = RepoLiveData(repoRepository, owner = username)
         }
         return map[username]
     }
-
 
 }
